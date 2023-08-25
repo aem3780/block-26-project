89910d3cfabd9705e178bff74dd14e3194d12bde
Allie Maus <aem3780@rit.edu>
2023-08-25 14:42:55 -0400

    Creating row and list components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import ContactList from './components/ContactList'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <ContactList></ContactList>
+      </div>
+    </>
+  )
+}
+
+export default App
