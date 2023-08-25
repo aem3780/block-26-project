16de92ec1f994ce2e219622b4e42e5dcbe6a9f2a
Allie Maus <aem3780@rit.edu>
2023-08-25 15:26:42 -0400

    Creating selected contact view

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-import './App.css'
-import ContactList from './components/ContactList'
-
-function App() {
-
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-      <div>
-      <ContactList></ContactList>
-      </div>
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
-  )
+  );
 }
-
-export default App
